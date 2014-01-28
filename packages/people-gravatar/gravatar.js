d4a62e30e2ef899350f872adcdf6474a379324ae
Andreas Marienborg <andreas.marienborg@gmail.com>
2014-01-28 18:03:09 +0700

    Add our gravatar package

---
+++ packages/people-gravatar/gravatar.js
@@ -0,0 +1,26 @@
+Gravatar = (function() {
+  function Gravatar(email) {
+    this.email = email;
+    if (this.email) {
+        this.hash = CryptoJS.MD5(this.email.trim().toLowerCase());
+    } else {
+        this.hash = "00000000000000000000000000000000";
+    }
+  }
+
+  Gravatar.prototype.url = function(fallback) {
+      console.log("in url", fallback, this);
+      if (!fallback) {
+          if (this.email) {
+              fallback = "retro";
+          } else {
+              fallback = "blank";
+          }
+      }
+      url = '//www.gravatar.com/avatar/' + this.hash + "?d=" + fallback;
+      return url;
+  };
+
+  return Gravatar;
+
+})();
